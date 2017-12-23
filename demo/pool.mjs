81aec89ed73a0f4f17f835899c048140de69ee80
Kim Dalsgaard <kim@kimdalsgaard.com>
2017-12-23 08:48:39 +0100

    Bugfix

--- demo/pool.mjs
+++ demo/pool.mjs
@@ -2,14 +2,16 @@ import { PromisePool } from '..';
 
 const pool = new PromisePool();
 
-process.nextTick(() => console.log('bar'));
+process.nextTick(() => console.log('#2'));
 
 pool.promise(30, 30).then(console.log);
 pool.promise(10, 10).then(p => {
   console.log(p);
-  process.nextTick(() => console.log('baz'));
+  process.nextTick(() => console.log('#3'));
 });
 pool.promise(20, 20).then(console.log);
 
-pool.fulfill();
-console.log('foo');
+pool.fulfill().then(() => {
+  console.log('#4');
+});
+console.log('#1');
